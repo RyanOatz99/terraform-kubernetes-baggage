b4cb3edff5dccddecca863210d52e463b2cd5700
Stuart Harland <stuartharland@Stuarts-MacBook-Pro.local>
2019-12-16 15:21:13 +0000

    Fix Baggage config to make it more generic

--- baggage_config.tf
+++ baggage_config.tf
@@ -17,130 +17,73 @@ resource "random_string" "api_key" {
   }
 }
 
+resource "random_string" "config_name" {
+  length      = 8
+  special     = false
+  min_lower   = 2
+  upper       = false
+  min_numeric = 2
+  keepers = {
+    "baggage.rb"        = local.baggage_rb
+    "puma.rb"           = local.puma_rb
+    "authenticator.txt" = local.authenticator_txt
+  }
+}
+
+
 resource "kubernetes_secret" "config" {
 
   metadata {
-    name      = local.instance_name
+    name      = "${local.instance_name}-${random_string.config_name.result}"
     namespace = local.namespace
   }
 
   type = "Opaque"
+
   data = {
-    "authenticator.txt" = data.null_data_source.config.outputs["authenticator_txt"]
-    "puma.rb"           = data.null_data_source.config.outputs["puma_rb"]
-    "baggage.rb"        = data.null_data_source.config.outputs["baggage_rb"]
+    "authenticator.txt" = local.authenticator_txt
+    "puma.rb"           = local.puma_rb
+    "baggage.rb"        = local.baggage_rb
   }
 }
 
-data "null_data_source" "config" {
-  inputs = {
-    authenticator_txt = "${random_string.api_key.result}:${random_string.authenticator_key.result}:read,write"
+locals {
+  policies = [
+    for bucket_name, bucket_details in data.terraform_remote_state.storage.outputs.buckets : <<EOF
+policy '${local.instance_name}.${bucket_name}' do
+  read do
+    backend 'fog',
+            fog_options: { provider: 'google',
+                           google_project: '${data.terraform_remote_state.storage.outputs.storage_account_project}',
+                           google_json_key_string: '${data.terraform_remote_state.storage.outputs.private_key_json}'},
+            scope: '${bucket_details}'
+  end
+  write do
+    sync 'fog',
+         fog_options: { provider: 'google',
+                        google_project: '${data.terraform_remote_state.storage.outputs.storage_account_project}',
+                        google_json_key_string: '${data.terraform_remote_state.storage.outputs.private_key_json}'},
+         scope: '${bucket_details}'
+  end
+end
+EOF
+  ]
+  authenticator_txt = "${random_string.api_key.result}:${random_string.authenticator_key.result}:read,write"
 
-    puma_rb = <<EOF
+  puma_rb = <<EOF
 threads ${var.min_threads},${var.max_threads}
 workers ${var.workers}
 preload_app!
 EOF
 
-    baggage_rb = <<EOF
-
+  baggage_rb = <<EOF
 module Baggage
   MEMCACHE_SERVERS = ENV['MEMCACHE_SERVER_PODS'].split(',').freeze
 end
 
 hostname 'baggage-${var.environment}-${var.client_name}-wk.livelinklabs.com'
-
-policy '${var.environment}.${var.client_name}.prints' do
-  cache servers: Baggage::MEMCACHE_SERVERS
-
-  authenticator type: :file, file: 'config/authenticator.txt'
-  read do
-    backend 'fog',
-            fog_options: { provider: 'google',
-                           google_project: '${data.terraform_remote_state.storage.outputs.storage_account_project}',
-                           google_json_key_string: '${data.terraform_remote_state.storage.outputs.private_key_json}'},
-            scope: '${data.terraform_remote_state.storage.outputs.buckets["prints"]}'
-  end
-
-  write do
-    sync 'fog',
-         fog_options: { provider: 'google',
-                        google_project: '${data.terraform_remote_state.storage.outputs.storage_account_project}',
-                        google_json_key_string: '${data.terraform_remote_state.storage.outputs.private_key_json}'},
-         scope: '${data.terraform_remote_state.storage.outputs.buckets["prints"]}'
-  end
-
-  delete do
-    sync 'fog',
-         fog_options: { provider: 'google',
-                        google_project: '${data.terraform_remote_state.storage.outputs.storage_account_project}',
-                        google_json_key_string: '${data.terraform_remote_state.storage.outputs.private_key_json}'},
-         scope: '${data.terraform_remote_state.storage.outputs.buckets["prints"]}'
-  end
-end
-
-policy '${var.environment}.${var.client_name}.prints-cache' do
-  cache servers: Baggage::MEMCACHE_SERVERS
-
-  authenticator type: :file, file: 'config/authenticator.txt'
-
-  read do
-    backend 'fog',
-            fog_options: { provider: 'google',
-                           google_project: '${data.terraform_remote_state.storage.outputs.storage_account_project}',
-                           google_json_key_string: '${data.terraform_remote_state.storage.outputs.private_key_json}'},
-            scope: '${data.terraform_remote_state.storage.outputs.buckets["prints_cache"]}'
-  end
-
-  write do
-    auth
-    sync 'fog',
-          fog_options: { provider: 'google',
-                         google_project: '${data.terraform_remote_state.storage.outputs.storage_account_project}',
-                         google_json_key_string: '${data.terraform_remote_state.storage.outputs.private_key_json}'},
-          scope: '${data.terraform_remote_state.storage.outputs.buckets["prints_cache"]}'
-  end
-
-  delete do
-    auth
-    sync 'fog',
-          fog_options: { provider: 'google',
-                         google_project: '${data.terraform_remote_state.storage.outputs.storage_account_project}',
-                         google_json_key_string: '${data.terraform_remote_state.storage.outputs.private_key_json}'},
-          scope: '${data.terraform_remote_state.storage.outputs.buckets["prints_cache"]}'
-  end
-end
-
-policy '${var.environment}.${var.client_name}.albums' do
-  cache servers: Baggage::MEMCACHE_SERVERS
-
-  authenticator type: :file, file: 'config/authenticator.txt'
-  read do
-    backend 'fog',
-            fog_options: { provider: 'google',
-                           google_project: '${data.terraform_remote_state.storage.outputs.storage_account_project}',
-                           google_json_key_string: '${data.terraform_remote_state.storage.outputs.private_key_json}'},
-                           scope: '${data.terraform_remote_state.storage.outputs.buckets["albums"]}'
-  end
-
-  write do
-    sync 'fog',
-         fog_options: { provider: 'google',
-                        google_project: '${data.terraform_remote_state.storage.outputs.storage_account_project}',
-                        google_json_key_string: '${data.terraform_remote_state.storage.outputs.private_key_json}'},
-         scope: '${data.terraform_remote_state.storage.outputs.buckets["albums"]}'
-  end
-
-  delete do
-    sync 'fog',
-         fog_options: { provider: 'google',
-                        google_project: '${data.terraform_remote_state.storage.outputs.storage_account_project}',
-                        google_json_key_string: '${data.terraform_remote_state.storage.outputs.private_key_json}'},
-                        scope: '${data.terraform_remote_state.storage.outputs.buckets["albums"]}'
-  end
-end
+${join("\n", local.policies)}
 EOF
 
-  }
 }
 
